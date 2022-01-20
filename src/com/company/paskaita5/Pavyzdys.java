d593d2dee27c7a9461650034ecbf66c7f6f94913
andriuskemeris <andrius@inventi.lt>
2022-01-20 18:01:13 +0200

    5 paskaita

---
+++ src/com/company/paskaita5/Pavyzdys.java
@@ -0,0 +1,17 @@
+package com.company.paskaita5;
+
+public class Pavyzdys {
+
+    public static void main(String[] args) {
+        atspausdinkJeiNelyginis(3);
+        atspausdinkJeiNelyginis(2);
+    }
+
+    public static void atspausdinkJeiNelyginis(int skaicius) {
+        if (skaicius % 2 == 0) {
+            return;
+        }
+
+        System.out.println(skaicius);
+    }
+}
---
+++ src/com/company/paskaita5/Uzduotis1.java
@@ -0,0 +1,22 @@
+package com.company.paskaita5;
+
+public class Uzduotis1 {
+
+    public static void main(String[] args) {
+        m1();
+    }
+
+    public static void m1() {
+        System.out.println("Pirmas metodas");
+        m2();
+    }
+
+    public static void m2() {
+        System.out.println("Antras metodas");
+        m3();
+    }
+
+    public static void m3() {
+        System.out.println("Trečias metodas");
+    }
+}
