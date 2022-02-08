7f7f173ae59c9ebe612fb091b31a5ba167c2b295
andriuskemeris <andrius@inventi.lt>
2022-02-08 18:02:57 +0200

    paskaita 12: protected example

---
+++ src/com/company/paskaita12/protectedexmaple/Main.java
@@ -0,0 +1,13 @@
+package com.company.paskaita12.protectedexmaple;
+
+import com.company.paskaita12.protectedexmaple.Studentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Studentas studentas = new Studentas("Andrius", "Kemeris", "123");
+
+        System.out.println(studentas);
+    }
+
+}
