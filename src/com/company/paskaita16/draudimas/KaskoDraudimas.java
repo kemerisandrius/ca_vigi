47754b142865fd611a78a376d6afe208d35c6ab9
andriuskemeris <andrius@inventi.lt>
2022-02-14 20:23:08 +0200

    paskaita 16: castai

---
+++ src/com/company/paskaita16/draudimas/KaskoDraudimas.java
@@ -0,0 +1,14 @@
+package com.company.paskaita16.draudimas;
+
+public class KaskoDraudimas extends AutomobilioDraudimas {
+    private double francize;
+
+    public KaskoDraudimas(String apdraustojoVardas, double draudimoSuma, String automobilioValstybiniaiNr, double francize) {
+        super(apdraustojoVardas, draudimoSuma, automobilioValstybiniaiNr);
+        this.francize = francize;
+    }
+
+    public double getFrancize() {
+        return francize;
+    }
+}
