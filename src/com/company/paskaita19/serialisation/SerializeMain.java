aba01e77d3a7094db7dd8207ce18f723e6f841d4
andriuskemeris <andrius@inventi.lt>
2022-02-17 19:38:57 +0200

    paskaita 19: serialization

---
+++ src/com/company/paskaita19/serialisation/SerializeMain.java
@@ -0,0 +1,19 @@
+package com.company.paskaita19.serialisation;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+public class SerializeMain {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //Issaugoti suserializuota objekta faile
+        FileOutputStream outputStream = new FileOutputStream("files/person_a");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+        Person person = new Person("Petras", "Kurmelis");
+        objectOutputStream.writeObject(person);
+    }
+}
